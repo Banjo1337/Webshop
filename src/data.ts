80e92284062a3fd13f000c066b4e9c7288737ac9
AlannAndre <80alaand@skola.boras.se>
2022-09-01 11:11:29 +0200

    Create CartContext

--- src/data.ts
+++ src/data.ts
@@ -1,3 +1,9 @@
 export {};
 
+export interface Book {
+    bookId: string;
+    bookTitle: string;
+    bookPrice: number;
+}
+
 export function getBook(bookId: string) {}
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import CartProvider from "./contexts/cartContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </CartProvider>
   </React.StrictMode>
 );
