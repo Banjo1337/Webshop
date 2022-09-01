77aa7962293c439c34af78b127c3b60fffa7e1b6
Robin Keturi <Robin.Keturi@gmail.com>
2022-09-01 15:34:27 +0200

    more styling to come

--- src/pages/ShoppingCart.tsx
+++ src/pages/ShoppingCart.tsx
@@ -1,14 +1,56 @@
-import { NavLink } from "react-router-dom";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { useCart } from '../contexts/CartContext';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Button } from '@mui/material';
 
-function ShoppingCart() {
+export default function BasicTable() {
+  const { cart, removeFromCart } = useCart();
   return (
-    <main style={{ top: "444px", background: "lightblue" }}>
-      <p>shoppingcart test</p>
-      <NavLink to="/checkout">
-        <button>Checkout</button>
-      </NavLink>
+    <main style={{maxWidth: "1000px", margin: "0 auto"}}>
+      <TableContainer component={Paper} sx={{display: "flex", flexDirection: "column"}}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Title</TableCell>
+              <TableCell align="right">Author</TableCell>
+              <TableCell align="right">Genre</TableCell>
+              <TableCell align="right">Price</TableCell>
+              <TableCell align="right">Remove</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {cart.map((product) => (
+              <TableRow
+                key={product.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {product.title}
+                </TableCell>
+                <TableCell align="right">{product.author}</TableCell>
+                <TableCell align="right">{product.category}</TableCell>
+                <TableCell align="right">{product.price}</TableCell>
+                <TableCell align="right">
+                  <DeleteIcon 
+                    sx={{color: "#F5425D"}}
+                    onClick={() => removeFromCart(product)}
+                  />
+                  </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <Button variant="contained" color="success" size="large">
+          Check out
+        </Button>
+      </TableContainer>
+
     </main>
   );
 }
-
-export default ShoppingCart;
