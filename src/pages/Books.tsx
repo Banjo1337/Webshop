6a4dae580ffe12dfcaf9778642db458dfab42381
LuminTompa <lumincob@gmail.com>
2022-09-01 15:12:31 +0200

    implement mui cards and button

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,15 +1,51 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
 import { useState } from "react";
+import ShopButton from "../components/ShopButton";
 import { mockedProducts } from "../data";
 
 function Books() {
   const [books] = useState(mockedProducts);
 
   return (
-    <main style={{ display: "flex", gap: "1rem" }}>
+    <main
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        padding: "1rem",
+      }}
+    >
       {books.map((book) => (
-        <div key={book.id}>
-          <img src={book.image} alt={book.title + " cover"} />
-          <h4>{book.title}</h4>
+        <div key={book.id} style={{ padding: ".5rem" }}>
+          <Card
+            sx={{
+              maxWidth: "12rem",
+              minHeight: "30rem",
+            }}
+          >
+            <CardMedia
+              component="img"
+              height="300"
+              image={book.image}
+              alt={book.title + " cover"}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="body2">
+                {book.title}
+              </Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                style={{ paddingBottom: ".3rem" }}
+              >
+                {book.author}
+              </Typography>
+              <ShopButton>{book.price} kr</ShopButton>
+            </CardContent>
+          </Card>
         </div>
       ))}
     </main>
