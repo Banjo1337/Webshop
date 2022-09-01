85bf0f1067827f7dbf4fca5469bd2e8853d1891c
Robin Keturi <Robin.Keturi@gmail.com>
2022-09-01 11:01:30 +0200

    added product context, using a temporary interface for product

---
+++ src/contexts/ProductContext.tsx
@@ -0,0 +1,54 @@
+import { createContext, ReactNode, useEffect, useState, useContext } from "react";
+
+interface Product {
+    id: string
+}
+
+interface ContextValue {
+	products: Product[];
+	addProduct: (product: Product) => void;
+	removeProduct: (product: Product) => void;
+    editProduct:  (product: Product) => void;
+}
+
+const ProductContext = createContext<ContextValue>(({
+	products:[],
+	addProduct: () => {},
+	removeProduct: () => {},
+    editProduct:  () => {},
+}));
+
+interface Props {
+	children: ReactNode;
+}
+
+function ProductProvider({children}: Props) {
+    const [products, setProducts] = useState<Product[]>(JSON.parse(localStorage.getItem("products")!) || []);
+
+    useEffect(() => {
+        localStorage.setItem("products", JSON.stringify(products))
+    }, [products])
+
+	function addProduct(product: Product) {
+        setProducts(prevState => [...prevState, product]);
+	}
+
+	function removeProduct(product: Product) {
+        setProducts(prevState => prevState.filter(p => p.id !== product.id));
+	}
+
+	function editProduct(product: Product) {
+        setProducts(prevState => prevState.map(p => p.id !== product.id ? p : product));
+	}
+
+    return <ProductContext.Provider value={{products, addProduct, removeProduct, editProduct}}>{children}</ProductContext.Provider>;
+
+
+}
+
+export default function useProduct() {
+    return useContext(ProductContext);
+};
+
+
+
