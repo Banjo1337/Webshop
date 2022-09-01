88e91a1b4ff1becf2840c6f8c1a2abe704b8b603
Fredrik Gröndahl <contact@fredrikgrondahl.com>
2022-09-01 10:48:53 +0200

    added imgages and product data

---
+++ src/components/Models.tsx
@@ -0,0 +1,17 @@
+export interface Product {
+  id: string;
+  price: number;
+  title: string;
+  description: string;
+  author: string;
+  image: string;
+  category: string;
+}
+
+//Kategorier:
+//Roman
+//Deckare
+//Kokbok
+//Barnbok
+//Träning
+//Fakta
