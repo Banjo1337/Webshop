a8c06d3175592349bdabd0387f8fc200165bde8d
AlannAndre <80alaand@skola.boras.se>
2022-09-01 22:38:11 +0200

    Admin icon added to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
 import bokmal from "../assets/bokmal.png";
 import Badge from '@mui/material/Badge';
 import { useCart } from "../contexts/CartContext";
@@ -17,52 +18,76 @@ export default function ResponsiveAppBar () {
   }
   return (
     <>
-      <AppBar position="static"
+      <AppBar
+        position="static"
         elevation={15}
         sx={{
-        background: "linear-gradient(0deg, rgba(172,172,172,0.7903536414565826) 0%, rgba(40,221,255,0.7791491596638656) 2%, rgba(40,130,255,1) 71%)",
-
-      }}>
-        <Container maxWidth="xl" sx={{height: "55px"}}>
-          <Box sx={{
+          background:
+            "linear-gradient(0deg, rgba(172,172,172,0.7903536414565826) 0%, rgba(40,221,255,0.7791491596638656) 2%, rgba(40,130,255,1) 71%)",
+        }}
+      >
+        <Container maxWidth="xl" sx={{ height: "55px" }}>
+          <Box
+            sx={{
               height: "100%",
               display: "flex",
-              justifyContent: "space-between"
-            }}>
-              <Link to="/" style={linkStyle}>
-                  <Box sx={{
-                    display: "flex",
-                    alignItems: "center"
-                  }}>
-                    <Box
-                      component="img"
-                      sx={{
-                        width: "40px"
-                      }}
-                      src={bokmal}
-                    />
-                    <Typography
-                      variant="h6"
-                      component="p"
-                      sx={{
-                        fontWeight: "300",
-                        letterSpacing: '.3rem',
-                        marginLeft: "0.3rem"
-                      }}
-                    >
-                      bokmal.se
-                    </Typography>
-                  </Box>
-              </Link>
-            <Link to="shoppingcart" style={linkStyle}>
-              <Badge color="secondary" overlap="circular" badgeContent={cart.length}>
-                <ShoppingCartIcon sx={{
-                  alignSelf: "center",
-                  fontSize: "2rem"
-                }}/>  
-              </Badge>
-              
+              justifyContent: "space-between",
+            }}
+          >
+            <Link to="/" style={linkStyle}>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <Box
+                  component="img"
+                  sx={{
+                    width: "40px",
+                  }}
+                  src={bokmal}
+                />
+                <Typography
+                  variant="h6"
+                  component="p"
+                  sx={{
+                    fontWeight: "300",
+                    letterSpacing: ".3rem",
+                    marginLeft: "0.3rem",
+                  }}
+                >
+                  bokmal.se
+                </Typography>
+              </Box>
             </Link>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                width: "70px",
+                justifyContent:"space-between"
+              }}
+            >
+              <Link to="shoppingcart" style={linkStyle}>
+                <Badge color="secondary" overlap="circular" badgeContent={cart.length}>
+                  <ShoppingCartIcon
+                    sx={{
+                      alignSelf: "center",
+                      fontSize: "2rem",
+                    }}
+                  />
+                </Badge>
+              </Link>
+              <Link to="admincms" style={linkStyle}>
+                <AdminPanelSettingsIcon
+                  sx={{
+                    alignSelf: "center",
+                    fontSize: "2rem",
+                  }}
+                />
+              </Link>
+            </Box>
           </Box>
         </Container>
       </AppBar>
