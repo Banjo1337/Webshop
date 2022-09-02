912df505a73a5cf745316ac2fd8bdd3a53c050d0
LuminTompa <lumincob@gmail.com>
2022-09-02 16:06:56 +0200

    initial category filtering

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -6,53 +6,82 @@ import { mockedProducts } from "../data";
 
 function Books() {
   const [books] = useState(mockedProducts);
+  const [filteredBooks, setFilteredBooks] = useState(books);
+
+  const categories = [...new Set(books.map((book) => book.category))];
 
   return (
     <main
       style={{
         display: "flex",
-        flexWrap: "wrap",
+        flexDirection: "row",
         padding: "1rem",
       }}
     >
-      {books.map((book) => (
-        <div
-          key={book.id}
-          style={{
-            padding: ".5rem",
-          }}
-        >
-          <Card
-            sx={{
-              maxWidth: "12rem",
-              minHeight: "30rem",
+      <div
+        style={{
+          background: "lightblue",
+          display: "flex",
+          flexDirection: "column",
+          flex: 1,
+          padding: ".4rem",
+        }}
+      >
+        {categories.map((category) => (
+          <div key={categories.indexOf(category)}>
+            <h4 style={{ margin: 0, padding: ".5rem" }}>{category}</h4>
+          </div>
+        ))}
+      </div>
+      <div
+        style={{
+          background: "lightyellow",
+          display: "flex",
+          flexWrap: "wrap",
+          flex: 7,
+        }}
+      >
+        {books.map((book) => (
+          <div
+            key={book.id}
+            style={{
+              padding: ".5rem",
             }}
           >
-            <Link to={`/book/${book.id}`}>
-              <CardMedia
-                component="img"
-                height="300"
-                image={book.image}
-                alt={book.title + " cover"}
-              />
-            </Link>
-            <CardContent>
-              <Typography gutterBottom variant="body2">
-                {book.title}
-              </Typography>
-              <Typography
-                variant="body2"
-                color="text.secondary"
-                style={{ paddingBottom: ".3rem" }}
-              >
-                {book.author}
-              </Typography>
+            <Card
+              sx={{
+                maxWidth: "12rem",
+                minHeight: "30rem",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <Link to={`/book/${book.id}`}>
+                <CardMedia
+                  component="img"
+                  height="300"
+                  image={book.image}
+                  alt={book.title + " cover"}
+                />
+              </Link>
+              <CardContent>
+                <Typography gutterBottom variant="body2">
+                  {book.title}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  style={{ paddingBottom: ".3rem" }}
+                >
+                  {book.author}
+                </Typography>
 
-              <ShopButton>{book.price} kr</ShopButton>
-            </CardContent>
-          </Card>
-        </div>
-      ))}
+                <ShopButton>{book.price} kr</ShopButton>
+              </CardContent>
+            </Card>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
