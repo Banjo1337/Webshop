0f169b9396cf4692291ec6fac3a1defa8b19ac2b
AlannAndre <80alaand@skola.boras.se>
2022-09-02 10:16:13 +0200

    Link added to the book cover to go to book details

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,3 +1,4 @@
+import { Link, Outlet } from "react-router-dom";
 import { useState } from "react";
 import { mockedProducts } from "../data";
 
@@ -8,7 +9,9 @@ function Books() {
     <main style={{ display: "flex", gap: "1rem" }}>
       {books.map((book) => (
         <div key={book.id}>
-          <img src={book.image} alt={book.title + " cover"} />
+          <Link to={`/book/${book.id}`}>
+            <img src={book.image} alt={book.title + " cover"} />
+          </Link>
           <h4>{book.title}</h4>
         </div>
       ))}
