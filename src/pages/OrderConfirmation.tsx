a4537f0d6a1623c664a22ea9b33641a6253ee22b
AlannAndre <80alaand@skola.boras.se>
2022-09-02 18:08:40 +0200

    add order confirmation page

---
+++ src/pages/OrderConfirmation.tsx
@@ -0,0 +1,10 @@
+function OrderConfirmation() {
+  return (
+    <main>
+      <h2>OrderConfirmation</h2>
+      <h3>Show order details and user data here</h3>
+    </main>
+  );
+}
+
+export default OrderConfirmation;
