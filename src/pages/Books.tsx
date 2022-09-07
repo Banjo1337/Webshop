f86098312a5fbc6a0c4d113bfa6bf3b68ab95f04
LuminTompa <lumincob@gmail.com>
2022-09-07 12:10:41 +0200

    get a new category filter

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,4 +1,8 @@
-import { Button, ButtonGroup, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { useState } from "react";
 import BookCard from "../components/BookCard";
 import ShopButton from "../components/ShopButton";
@@ -8,25 +12,34 @@ function Books() {
   const [books] = useState(mockedProducts);
   const [categoryFilter, setCategoryFilter] = useState("");
   const categories = [...new Set(mockedProducts.map((book) => book.category))];
-  const buttons = [
-    <Button key={-1} onClick={() => setCategoryFilter("")}>
-      Alla böcker
-    </Button>,
-    categories.map((c) => (
-      <Button key={categories.indexOf(c)} onClick={() => setCategoryFilter(c)}>
-        {c}
-      </Button>
-    )),
-  ];
+  const handleChange = (event: SelectChangeEvent) => {
+    setCategoryFilter(event.target.value);
+  };
 
   return (
-    <div style={{ padding: "1rem", display: "flex", flexDirection: "column", marginTop: "50px" }}>
-      <ButtonGroup sx={{ display: "flex", flexDirection: "row" }}>{buttons}</ButtonGroup>
+    <div style={{ padding: "1rem", display: "flex", flexDirection: "column", marginTop: "45px" }}>
+      <FormControl variant='standard' sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id='demo-simple-select-standard-label'>Välj en kategori</InputLabel>
+        <Select
+          labelId='demo-simple-select-standard-label'
+          id='demo-simple-select-standard'
+          value={categoryFilter}
+          onChange={handleChange}
+          label='Category'
+        >
+          <MenuItem value=''>Alla böcker</MenuItem>
+          {categories.map((c) => (
+            <MenuItem key={c} value={c}>
+              {c}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
       <Grid container spacing={1}>
         {books
           .filter((b) => b.category.toLowerCase().includes(categoryFilter.toLowerCase()))
           .map((book) => (
-            <Grid key={book.id} item xs={4} sm={3} md={2} lg={1.5}>
+            <Grid key={book.id} item xs={4} sm={3} md={2} lg={1.5} xl={1}>
               <BookCard book={book}>
                 <ShopButton product={book}>{book.price} kr</ShopButton>
               </BookCard>
