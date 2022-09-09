129b673f95d4d93c02b11b0aec2808ce2761d9bf
LuminTompa <lumincob@gmail.com>
2022-09-09 12:10:16 +0200

    switch table for a better responsive page

--- src/pages/AdminPages/AdminCMS.tsx
+++ src/pages/AdminPages/AdminCMS.tsx
@@ -1,45 +1,109 @@
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import RemoveIcon from "@mui/icons-material/Remove";
+import Box from "@mui/material/Box";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import DeleteIcon from "@mui/icons-material/Delete";
-import EditIcon from "@mui/icons-material/Edit";
-import AddIcon from "@mui/icons-material/Add";
-import RemoveIcon from "@mui/icons-material/Remove";
-import { Box } from "@mui/material";
-import { useProduct } from "../../contexts/ProductContext";
+import * as React from "react";
 import { Link, Outlet } from "react-router-dom";
+import { Book } from "../../components/Models";
+import { useProduct } from "../../contexts/ProductContext";
 
-export default function AdminCMS() {
-  const { products, removeProduct } = useProduct();
+const linkStyle = {
+  display: "flex",
+  textDecoration: "none",
+  color: "white",
+};
+
+function Row(product: Book) {
+  const [open, setOpen] = React.useState(false);
+  const { removeProduct } = useProduct();
   const formatter = new Intl.NumberFormat("sv-SE", {
     style: "currency",
     currency: "SEK",
   });
-  const linkStyle = {
-    display: "flex",
-    textDecoration: "none",
-    color: "white",
-  };
+
+  return (
+    <React.Fragment>
+      <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
+        <TableCell>
+          <IconButton aria-label='expand row' size='small' onClick={() => setOpen(!open)}>
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component='th' scope='row'>
+          <Box
+            component='img'
+            sx={{
+              width: "40px",
+            }}
+            src={product.image}
+          />
+        </TableCell>
+        <TableCell align='right'>{product.title}</TableCell>
+        <TableCell align='right'>
+          <Link to='updatebookadmin' state={{ id: product.id }} style={linkStyle}>
+            <EditIcon sx={{ color: "#F5425D", cursor: "pointer" }} />
+          </Link>
+          <DeleteIcon
+            sx={{ color: "#F5425D", cursor: "pointer" }}
+            onClick={() => removeProduct(product)}
+          />
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout='auto' unmountOnExit>
+            <Box sx={{ margin: 1 }}>
+              <Table size='small' aria-label='purchases'>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Id</TableCell>
+                    <TableCell>Price</TableCell>
+                    <TableCell align='right'>Description</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow key={product.id}>
+                    <TableCell component='th' scope='row'>
+                      {product.id}
+                    </TableCell>
+                    <TableCell>{formatter.format(product.price)}</TableCell>
+                    <TableCell align='right'>{product.description}</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
+
+export default function AdminCMS() {
+  const { products } = useProduct();
 
   return (
     <main style={{ display: "flex", flexDirection: "column" }}>
-      <h2>Bokmal.se - Admin page</h2>
       <Outlet />
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+      <TableContainer component={Paper} sx={{ pt: "5rem" }}>
+        <Table aria-label='collapsible table'>
           <TableHead>
             <TableRow>
-              <TableCell align='center'></TableCell>
-              <TableCell align='center'>Id</TableCell>
-              <TableCell align='center'>Title</TableCell>
-              <TableCell align='center'>Author</TableCell>
-              <TableCell align='center'>Genre</TableCell>
-              <TableCell align='center'>Price</TableCell>
-              <TableCell align='center'>Description</TableCell>
+              <TableCell />
+              <TableCell>Cover</TableCell>
+              <TableCell align='right'>Title</TableCell>
               <TableCell align='right'>
                 <Link to='addbookadmin' style={linkStyle}>
                   <AddIcon sx={{ color: "#F5425D", cursor: "pointer" }} />
@@ -52,32 +116,16 @@ export default function AdminCMS() {
           </TableHead>
           <TableBody>
             {products.map((product) => (
-              <TableRow key={product.id} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-                <TableCell align='right'>
-                  <Box
-                    component='img'
-                    sx={{
-                      width: "40px",
-                    }}
-                    src={product.image}
-                  />
-                </TableCell>
-                <TableCell align='center'>{product.id}</TableCell>
-                <TableCell align='center'>{product.title}</TableCell>
-                <TableCell align='center'>{product.author}</TableCell>
-                <TableCell align='center'>{product.category}</TableCell>
-                <TableCell align='center'>{formatter.format(product.price)}</TableCell>
-                <TableCell align='left'>{product.description}</TableCell>
-                <TableCell align='right'>
-                  <Link to='updatebookadmin' state={{ id: product.id }} style={linkStyle}>
-                    <EditIcon sx={{ color: "#F5425D", cursor: "pointer" }} />
-                  </Link>
-                  <DeleteIcon
-                    sx={{ color: "#F5425D", cursor: "pointer" }}
-                    onClick={() => removeProduct(product)}
-                  />
-                </TableCell>
-              </TableRow>
+              <Row
+                key={product.id}
+                id={product.id}
+                price={product.price}
+                title={product.title}
+                description={product.description}
+                author={product.author}
+                image={product.image}
+                category={product.category}
+              />
             ))}
           </TableBody>
         </Table>
