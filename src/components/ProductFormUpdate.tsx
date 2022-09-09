1043960cfe9f02172515ce1f81b8718567501231
AlannAndre <80alaand@skola.boras.se>
2022-09-09 09:32:30 +0200

    ändrar genre

--- src/components/ProductFormUpdate.tsx
+++ src/components/ProductFormUpdate.tsx
@@ -35,12 +35,12 @@ function ProductForm(props: Props) {
   }
 
   const options = [
-    { value: "roman", label: "Roman", id: 1 },
-    { value: "deckare", label: "Deckare", id: 2 },
-    { value: "kokbok", label: "Kokbok", id: 3 },
-    { value: "barnbok", label: "Barnbok", id: 4 },
-    { value: "träning", label: "Träning", id: 5 },
-    { value: "fakta", label: "Fakta", id: 6 },
+    { value: "Roman", label: "Roman", id: 1 },
+    { value: "Deckare", label: "Deckare", id: 2 },
+    { value: "Kokbok", label: "Kokbok", id: 3 },
+    { value: "Barnbok", label: "Barnbok", id: 4 },
+    { value: "Träning", label: "Träning", id: 5 },
+    { value: "Fakta", label: "Fakta", id: 6 },
   ];
 
   const formik = useFormik<ProductCreate>({
