92a2a12bbaaecadcc977e405e9c6e2dd03e1e3fc
Robin Keturi <Robin.Keturi@gmail.com>
2022-09-14 11:36:38 +0200

    splitted mui to own chunk

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules/@mui')) 
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+        },
+      },
+    },
+  },
   plugins: [react()],
 });
