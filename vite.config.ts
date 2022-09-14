8bbabf363cb008b1ac6302127956c22607cff99b
Robin Keturi <Robin.Keturi@gmail.com>
2022-09-14 11:37:19 +0200

    splitting all packages to own chunk via plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules/@mui')) 
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-        },
-      },
-    },
-  },
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
 });
