eee23ed20f7bea2b0d564bb66e1d7783cf161d7c
Fredrik Gröndahl <contact@fredrikgrondahl.com>
2022-09-16 15:06:47 +0200

    edit footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,56 +1,36 @@
-import { CSSProperties } from "react";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import Twitter from "@mui/icons-material/Twitter";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { CSSProperties } from "react";
 
 export default function Footer() {
   return (
-    <footer style={footerStyle}>
-      <div style={divStyle}>
+    <footer>
+      <div style={{ alignSelf: "center" }}>
+        <p>Tel: +46 725 56 55</p>
+      </div>
+      <div>
         <ul style={ulStyle}>
-          <li style={liStyle}>
+          <li style={{ paddingRight: "1rem" }}>
             <Twitter />
           </li>
-          <li style={liStyle}>
+          <li>
             <FacebookIcon />
           </li>
-          <li style={liStyle}>
+          <li style={{ paddingLeft: "1rem" }}>
             <LinkedInIcon />
           </li>
         </ul>
-        <ul style={ulStyle}>
-          <li style={liStyle}>Tel: +46 725 56 55</li>
-        </ul>
-        <ul style={ulStyle}>
-          <li style={liStyle}>Mail: contact@bokmal.se</li>
-        </ul>
+      </div>
+      <div style={{ alignSelf: "center" }}>
+        <p>Mail: contact@bokmal.se</p>
       </div>
     </footer>
   );
 }
 
-const footerStyle: CSSProperties = {
-  display: "flex",
-  width: "100%",
-  alignContent: "space-between",
-  background: "linear-gradient(rgba(40,130,255,1), rgba(40,130,255,0.7))",
-  color: "#f5f5f5",
-  flexDirection: "column",
-  fontFamily: "Roboto, Helvetica, Arial, sans-serif",
-  fontSize: "14px",
-};
-
-const divStyle: CSSProperties = {
-  alignSelf: "center",
-};
-
 const ulStyle: CSSProperties = {
   display: "flex",
-  justifyContent: "space-around",
-  alignContent: "center",
-  alignItems: "center",
-};
-
-const liStyle: CSSProperties = {
   listStyle: "none",
+  alignSelf: "center",
 };
