b3cb11feea250e533bc70bc18df672ef719fa792
Robin Keturi <Robin.Keturi@gmail.com>
2022-09-16 17:36:46 +0200

    had to break out checkoutrow because you're not allowed to use hooks in map

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -6,24 +6,25 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import { Box } from "@mui/material";
 import Paper from "@mui/material/Paper";
 import EmptyShoppingCart from "../components/EmptyShoppingCart";
-import useFallBackImage from "../hooks/useFallBackImage";
-import useCurrencyFormatter from "../hooks/useCurrencyFormatter";
+import getFormattedCurrency from "../utils/getFormattedCurrency";
+import CheckoutRow from "../components/CheckoutRow";
+
 
 function Checkout() {
   const { cart } = useCart();
 
-  const totalPrice = cart.reduce(
+  const totalPrice = getFormattedCurrency(cart.reduce(
     (totalPrice, product) => (totalPrice = totalPrice + product.price * product.number),
     0,
-  );
+  ));
+  
 
   if (cart.length === 0) {
     return <EmptyShoppingCart />;
   }
-
+  
   return (
     <main
       style={{
@@ -44,32 +45,15 @@ function Checkout() {
           </TableHead>
           <TableBody>
             {cart.map((product) => (
-              <TableRow key={product.id} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-                <TableCell align='right'>
-                  <Box
-                    component='img'
-                    sx={{
-                      width: "40px",
-                    }}
-                    src={useFallBackImage(product.image)}
-                  />
-                </TableCell>
-                <TableCell component='th' scope='row'>
-                  <b>{product.title}</b> ({product.category}) <br />
-                  {product.author}
-                  <br />
-                  Antal: {product.number}
-                </TableCell>
-                <TableCell align='left'>{useCurrencyFormatter(product.price)}</TableCell>
-              </TableRow>
-            ))}
+                <CheckoutRow key={product.id} product={product} />
+            ))}    
           </TableBody>
         </Table>
         <Table sx={{ minWidth: 360 }} aria-label='simple table'>
           <TableHead>
             <TableRow>
               <TableCell></TableCell>
-              <TableCell align='right'>Totalt: {useCurrencyFormatter(totalPrice)}</TableCell>
+              <TableCell align='right'>Totalt: {totalPrice}</TableCell>
               <TableCell></TableCell>
             </TableRow>
           </TableHead>
